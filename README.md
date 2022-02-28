1ae519ff610307b7b40338b548b8b7377b9022a4
bilidly_exchange <connected.addy@gmail.com>
2022-02-28 16:24:19 +0700

    Hello World

---
+++ README.md
@@ -0,0 +1,17 @@
+## Bilidly
+
+![gameplay](images/monkey-pong.gif)
+
+## What is it?
+
+Bilidly is a fork of Andre Cronje's Solidly for protocols and users of the Binance Smart Chain.
+
+## Why build it?
+
+Solidly is a near-zero fee, efficient curve AMM for swapping pegged and non-pegged assets. We understand its model is bound to be replicated on every blockchain.
+
+## Sources
+
+[Solidly Smart Contracts](https://github.com/solidlyexchange/solidly)
+
+[Solidly Frontend](https://github.com/solidlyexchange/solidly.exchange)
---
+++ src/WebsiteOverlay.tsx
@@ -0,0 +1,28 @@
+import { VFC } from "react";
+//import { BiMenu } from "react-icons/bi";
+import { FaMedium, FaTelegram, FaTwitter } from "react-icons/fa";
+
+export const WebsiteOverlay: VFC = () => {
+  return (
+    <div className="flex flex-col absolute w-full h-full z-10 p-0 text-white">
+      <div className="flex font-bold flex-col my-auto h-full max-w-[36rem] backdrop-brightness-0 p-4">
+        <div className="uppercase text-8xl">LOADING...</div>
+        <div className="uppercase text-5xl mb-6">APES VS. PROTOCOL</div>
+        <div className="mb-6">
+          Bilidly is a roll-out of Solidly protocol on Binance Smart Chain
+        </div>
+        <div className="flex text-3xl gap-8 mt-auto">
+          <a href="https://twitter.com/bilidlyexchange" target="blank">
+            <FaTwitter />
+          </a>
+          <a href="https://medium.com/@Bilidly" target="blank">
+            <FaMedium />
+          </a>
+          <a href="https://twitter.com/bilidlyexchange" target="blank">
+            <FaTelegram />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
