b933dfe38a3f6c354bc04c4a059440e4d7a0d854
bilidly_exchange <connected.addy@gmail.com>
2022-02-28 18:47:04 +0700

    U Message

--- src/WebsiteOverlay.tsx
+++ src/WebsiteOverlay.tsx
@@ -6,11 +6,12 @@ export const WebsiteOverlay: VFC = () => {
   return (
     <div className="flex flex-col absolute w-full h-full z-10 p-0 text-white">
       <div className="flex font-bold flex-col my-auto h-full max-w-[36rem] backdrop-brightness-0 p-4">
-        <div className="uppercase text-8xl">LOADING...</div>
-        <div className="uppercase text-5xl mb-6">APES VS. PROTOCOL</div>
-        <div className="mb-6">
+        <div className="uppercase text-8xl mb-6 mt-60">LOADING<span className="dots"><span>.</span><span>.</span><span>.</span></span></div>
+        
+        <div className="uppercase text-5xl mb-12">APES VS. PROTOCOLS</div>
+        {/*<div className="mb-6">
           Bilidly is a roll-out of Solidly protocol on Binance Smart Chain
-        </div>
+        </div>*/}
         <div className="flex text-3xl gap-8 mt-auto">
           <a href="https://twitter.com/bilidlyexchange" target="blank">
             <FaTwitter />
