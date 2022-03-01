4744edbaafead5c348ccc9da49f2d12d284cffac
bilidly_exchange <connected.addy@gmail.com>
2022-03-01 13:57:34 +0700

    improve responsiveness

--- src/VideoBackground.tsx
+++ src/VideoBackground.tsx
@@ -9,7 +9,7 @@ export const VideoBackground: VFC = () => {
         <iframe
             width={VIDEO_WIDTH}
             height={VIDEO_HEIGHT}
-            src="https://www.youtube.com/embed/LgJpYOTll8U?autoplay=1&controls=0&mute=1&loop=1&playlist=LgJpYOTll8U&modestbranding=1&showinfo=0&start=50&enablejsapi=1&&widgetid=3"
+            src="https://www.youtube.com/embed/LgJpYOTll8U?autoplay=1&controls=0&showinfo=0&autohide=1&mute=1&loop=1&playlist=LgJpYOTll8U&modestbranding=1&showinfo=0&start=50&enablejsapi=1&&widgetid=3"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
